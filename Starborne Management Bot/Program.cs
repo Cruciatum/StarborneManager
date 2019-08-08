d89c2e5c341f59f80476223beb4901f4e6f910d8
Cruciatum <yorrick.dekempe@gmail.com>
2019-08-08 20:55:17 +0200

    Add project files.

---
+++ Starborne Management Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Starborne_Management_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
